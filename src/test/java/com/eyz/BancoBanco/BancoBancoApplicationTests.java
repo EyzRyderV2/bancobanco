9df0d0eef47242d1ad61b98ae80de604cb26115f
Gabriel Bessi <gabriel.bessi.gb@gmail.com>
2025-03-15 20:46:47 -0300

    first commit

---
+++ src/test/java/com/eyz/BancoBanco/BancoBancoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eyz.BancoBanco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoBancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
